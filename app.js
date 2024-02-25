02ac59f052a636c6856d6a93f5d99aa699d83abc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-25 08:03:22 +0430

    init

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function getData(url) {
     xhr.onreadystatechange = function () {
         if(xhr.readyState !== 4)return
         if(xhr.status === 200) {
-            console.log(xhr.responseText);
+            const response = JSON.parse(xhr.responseText)
         }
         else {
             console.log({
