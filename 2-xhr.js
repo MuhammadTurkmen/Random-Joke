d8434906f8b1021629346460d4715d55f53e2aa5
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-25 08:39:29 +0430

    init

--- 2-xhr.js
+++ 2-xhr.js
@@ -17,13 +17,7 @@ function getData(url) {
         xhr.onreadystatechange = function () {
             if(xhr.readyState !== 4)return
             if(xhr.status === 200) {
-                img.classList.add('shake-img')
-                const { value: joke } = JSON.parse(xhr.responseText)
-                content.textContent = joke
-                const random = Math.random() * 1000
-                setTimeout(() => {
-                    img.classList.remove('shake-img')
-                }, random)
+                
             }
             else {
                 console.log({
