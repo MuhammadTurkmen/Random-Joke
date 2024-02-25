4dc00c7c6facc0774543e95fcbbd3454399b2d9e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-25 10:08:47 +0430

    init

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ btn.addEventListener('click', () => {
     getData(URL)
 })
 
-function getData(url) { 
+function getData(url) {  
     const xhr = new XMLHttpRequest()
     xhr.open('GET', url)
     xhr.send()
     xhr.onreadystatechange = function () {     
-        if(xhr.readyState !== 4)return
+        if(xhr.readyState !== 4)return 
         if(xhr.status === 200) { 
             img.classList.add('shake-img') 
             const { value: joke } = JSON.parse(xhr.responseText) 
