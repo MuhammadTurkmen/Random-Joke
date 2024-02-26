9a3d52d82c90fae2289489671ef4762b8a1dc8db
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-26 10:12:33 +0430

    init

--- app.js
+++ app.js
@@ -7,28 +7,37 @@ const URL = 'https://api.chucknorris.io/jokes/random'
 
 btn.addEventListener('click', () => {
     getData(URL)
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err))
 })
 
-function getData(url) {  
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.send() 
-    xhr.onreadystatechange = function () {     
-        if(xhr.readyState !== 4)return 
-        if(xhr.status === 200) { 
-            img.classList.add('shake-img') 
-            const { value: joke } = JSON.parse(xhr.responseText) 
-            content.textContent = joke
-            const random = Math.random() * 1000
-            setTimeout(() => {
-                img.classList.remove('shake-img')
-            }, random)
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', url)
+        xhr.send()
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState !== 4)return
+            if(xhr.status === 200) {
+                resolve(xhr.responseText)
+            }
+            else {
+                reject({
+                    status: xhr.status,
+                    text: xhr.statusText
+                })
+            }
         }
-        else {
-            console.log({
-                status: xhr.status,
-                text: xhr.statusText
-            });
-        }
-    }
+        
+    })
+}
+
+function displayData(data) {
+    img.classList.add('shake-img')
+    const { value: joke } = JSON.parse(data)
+    content.textContent = joke
+    const random = Math.random() * 1000
+    setTimeout(() => {
+        img.classList.remove('shake-img')
+    }, random)
 }
