08a1f47b557b72e905a475cacab7466f34a7ac0b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-26 10:55:05 +0430

    init

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const URL = 'https://api.chucknorris.io/jokes/random'
 
 btn.addEventListener('click', async () => {
     try {
+        const data = await fetch(URL)
+        const response = await data.json()
+        displayData(response)
         
     } catch (error) {
         
     }
-    const data = await fetch(URL)
-    const response = await data.json()
-    displayData(response)
 })
 
 
